a4c5049d89d6c326a3107b4b5a551c4c1e2aa0af
shefegazarsina <azarsinashefeg4@gmail.com>
2023-04-01 21:19:49 -0400

    My second commit l

--- src/day17_Loop/PrintEachChar.java
+++ src/day17_Loop/PrintEachChar.java
@@ -1,7 +1,24 @@
 package day17_Loop;
 
+import java.util.Scanner;
+
 public class PrintEachChar {
     public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
 
+        String str = "java is great language"; // ---length(); --- > 4
+        //            0123
+
+
+        System.out.println(str.charAt(0));  // j
+        System.out.println(str.charAt(1));
+        System.out.println(str.charAt(2));
+        System.out.println(str.charAt(3));
+
+        System.out.println("-------------");
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+
+        }
     }
 }
