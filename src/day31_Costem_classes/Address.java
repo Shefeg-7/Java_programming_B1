7d9518f27e9da834a991ee52625d964ad9f65a58
shefegazarsina <azarsinashefeg4@gmail.com>
2023-04-19 22:26:23 -0400

    day31_costem_class_continue

---
+++ src/day31_Costem_classes/Address.java
@@ -0,0 +1,20 @@
+package day31_Costem_classes;
+
+public class Address {
+
+        String city;
+        String street;
+        String state;
+        int zipcode;
+
+
+        // Create a constructor that accepts 4 parameters with the same name as your instance variables. Then assign your local variables to you instance variables.
+        public Address (String street, String city, String state, int zipcode){
+            this.street = street;
+            this.city = city;
+            this.zipcode = zipcode;
+            this.state = state;
+        }
+
+    }
+
---
+++ src/day31_Costem_classes/UseThisKeyword.java
@@ -0,0 +1,11 @@
+package day31_Costem_classes;
+
+public class UseThisKeyword {
+
+
+    public static void main(String[] args) {
+        ThisKeyword obj = new ThisKeyword(300);
+        System.out.println(obj.a);
+
+    }
+}
