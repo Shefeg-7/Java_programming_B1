6c89b8231ca56e467ed816e0a29ef81da0e24e97
shefegazarsina <azarsinashefeg4@gmail.com>
2023-04-20 22:20:54 -0400

    day32_costem_class

---
+++ src/day32_work/UsingChain.java
@@ -0,0 +1,21 @@
+package day32_work;
+
+public class UsingChain {
+
+
+    public static void main(String[] args) {
+        ChainExample ch1 = new ChainExample();
+        ChainExample ch2 = new ChainExample("Test");
+
+
+        new ChainExample(4.2);
+
+        System.out.println("---------------------------");
+
+        new ChainExample(3.2);
+
+
+    }
+
+
+}
