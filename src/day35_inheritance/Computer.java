b8c8d1ab191503dce8cd26579c9b6266fde23727
shefegazarsina <azarsinashefeg4@gmail.com>
2023-04-27 21:30:27 -0400

    day35_inheritance

---
+++ src/day35_inheritance/Computer.java
@@ -0,0 +1,16 @@
+package day35_inheritance;
+
+public class Computer {
+
+
+
+    String os;
+    int memory;
+
+    public Computer(String os, int memory) {
+        this.os = os;
+        this.memory = memory;
+    }
+}
+
+
---
+++ src/day35_inheritance/Linux.java
@@ -0,0 +1,9 @@
+package day35_inheritance;
+
+
+    public class Linux extends Computer {
+        public Linux(int memory){
+            super("Linux", memory);
+        }
+    }
+
