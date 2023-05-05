3793f920ab22cc6caa3aa5f16b7b7886611015d5
shefegazarsina <azarsinashefeg4@gmail.com>
2023-05-04 22:04:27 -0400

    day38_work

---
+++ src/day38_b_polymorphism/animal/Wild.java
@@ -0,0 +1,11 @@
+package day38_b_polymorphism.animal;
+
+
+    public class Wild {
+        public static void main(String[] args) {
+
+            // Reference type --- > itself
+            Lizard lz = new Lizard();
+            lz.eat();
+        }
+    }
